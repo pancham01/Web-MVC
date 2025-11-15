066d57089344f4c69abe6a31e56a9cdeff9c1b7c
pancham01 <pancham9557@gmail.com>
2025-11-15 11:49:43 +0530

    Initial commit

---
+++ src/main/java/learning/spring/mvc/MyController.java
@@ -0,0 +1,17 @@
+package learning.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/")
+	public String greetings()
+	{
+		System.out.println("MyController.greetings()");
+		return "welcome";
+	}
+	
+
+}
---
+++ src/main/webapp/WEB-INF/JSP/welcome.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring-MVC</title>
+</head>
+<body bgcolor="green">
+	<h1>This is the first Spring MVC Program...........</h1>
+</body>
+</html>
